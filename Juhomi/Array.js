184ec1359093571b2e1ccf77e0bc48244cddd1a8
Abhiram-932 <77227232+Abhiram-932@users.noreply.github.com>
2021-12-15 17:15:10 +0530

    Add files via upload

---
+++ Juhomi/Array.js
@@ -0,0 +1,24 @@
+// Use the each method of Array to iterate over [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], and print out each value.
+
+// Map
+let arr =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let NewArray =arr.map(item=>item);
+console.log(NewArray);
+
+//filter
+let str =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let New =str.filter(item=>item);
+console.log(New);
+
+//forEach
+let array =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+array.forEach(item=>{
+    console.log(item)
+})
+
+
+
+
+ 
+
+
